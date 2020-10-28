fc926331335c0a611e0ddda0a036282af5797bc0
sahbiammar <sahbi.ammar.1995@gmail.com>
2020-10-28 22:18:20 +0100

    Updates

---
+++ precache-manifest.5eaa241d780af40b521a35286c889828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93440068e6faeca76539009095104c7c",
+    "url": "/React-Movie-App/index.html"
+  },
+  {
+    "revision": "3873175e0eebac6fb075",
+    "url": "/React-Movie-App/static/css/main.769b8888.chunk.css"
+  },
+  {
+    "revision": "e7c3471d0df1efa5fadf",
+    "url": "/React-Movie-App/static/js/2.cd8365d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Movie-App/static/js/2.cd8365d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3873175e0eebac6fb075",
+    "url": "/React-Movie-App/static/js/main.0155b65d.chunk.js"
+  },
+  {
+    "revision": "50abb06e6d6f2fa4e887",
+    "url": "/React-Movie-App/static/js/runtime-main.b0d34533.js"
+  }
+]);
